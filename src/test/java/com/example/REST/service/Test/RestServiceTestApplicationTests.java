4e5093bbdf507d80d93c18aeaa33a7e1d59089b0
MrGatitu <mrgatituh@gmail.com>
2024-06-29 18:34:39 +0300

    Module 1 Creating A REST service

---
+++ src/test/java/com/example/REST/service/Test/RestServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.REST.service.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
